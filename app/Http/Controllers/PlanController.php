6d740b86d73e0662d82ed02d8af4c0ffd0211c84
gaboliveiradev <g38789138@gmail.com>
2024-01-17 13:31:53 -0300

    feat: method store

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PlanModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PlanController extends Controller
 {
@@ -12,5 +13,28 @@ class PlanController extends Controller
         $plans = PlanModel::all();
 
         return response()->json($plans, 200);
+    }
+
+    public function store(Request $request) 
+    {
+        $request->validate([
+            'description' => 'required|string',
+            'days' => 'required|string',
+            'price' => 'required|string',
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            $plans = PlanModel::create($request->all());
+        } catch (\Throwable $e) {
+            DB::rollBack();
+
+            return response()->json(['errors' => ['database' => [$e->getMessage()]]], 404);
+        }
+
+        DB::commit();
+
+        return response()->json($plans, 201);
     }
 }
