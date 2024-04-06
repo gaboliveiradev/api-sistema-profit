86e5019e83399b4805d28eb271b5756eb1dfbc33
gaboliveiradev <g38789138@gmail.com>
2024-04-05 23:55:12 -0300

    novo método store

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -2,32 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plan;
+use App\Models\PlanModality;
 use App\Models\PlanModel;
+use App\Models\PlanPrice;
+use App\Models\PlanService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PlanController extends Controller
 {
-    public function index() 
+    /*public function index() 
     {
         $plans = PlanModel::whereNull('deleted_at')->orderBy('created_at', 'desc')->get();
 
         return response()->json($plans, 200);
-    }
+    }*/
 
     public function store(Request $request) 
     {
         $request->validate([
-            'id_gym' => 'required|integer',
-            'description' => 'required|string',
-            'days' => 'required|string',
-            'price' => 'required|string',
+            'id_business_partners' => 'required|integer',
+            'name' => 'required|string',
         ]);
 
         DB::beginTransaction();
 
         try {
-            $plans = PlanModel::create($request->all());
+            $plan = Plan::create([
+                'id_business_partner' => $request->get('id_business_partners'),
+                'name' => $request->get('name'),    
+            ]);
+
+            $idsServices = !empty($request->get('services')) ? $request->get('services') : [];
+
+            foreach ($idsServices as $idService) {
+                PlanService::create([
+                    'id_plan' => $plan->id,
+                    'id_service' => $idService
+                ]);
+            }
+
+            $idsModalities = !empty($request->get('modalities')) ? $request->get('modalities') : [];
+
+            foreach ($idsModalities as $idModality) {
+                PlanModality::create([
+                    'id_plan' => $plan->id,
+                    'id_modality' => $idModality,
+                    'period' => 1,
+                    'days' => 6,    
+                ]);
+            }
+
+            $prices = !empty($request->get('prices')) ? $request->get('prices') : [];
+
+            foreach ($prices as $price) {
+                PlanPrice::create([
+                    'id_plan' => $plan->id,
+                    'period' => $price['id'],
+                    'price' => $price['value'],
+                ]);
+            }
+
         } catch (\Throwable $e) {
             DB::rollBack();
 
@@ -36,10 +72,10 @@ class PlanController extends Controller
 
         DB::commit();
 
-        return response()->json($plans, 201);
+        return response()->json($plan, 201);
     }
 
-    public function destroy($id)
+    /*public function destroy($id)
     {
         $plan = PlanModel::find($id);
 
@@ -56,5 +92,5 @@ class PlanController extends Controller
         ]);
 
         return response()->json(['mensagem' => 'Plano excluído com sucesso'], 200);
-    }
+    }*/
 }
