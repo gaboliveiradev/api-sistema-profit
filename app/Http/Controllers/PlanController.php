8c1cf6a60e7f0abc70c1dc48bb65e6aaf3cea268
gaboliveiradev <g38789138@gmail.com>
2024-04-10 20:54:28 -0300

    feat: alter methood index plan

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Definitions\Plan\ModalityDefinition;
+use App\Definitions\Plan\PriceDefinition;
+use App\Definitions\Plan\ServiceDefinition;
+use App\Domains\Plan\ModalityDomain;
+use App\Domains\Plan\PriceDomain;
+use App\Domains\Plan\ServiceDomain;
 use App\Models\Plan;
 use App\Models\PlanModality;
 use App\Models\PlanModel;
@@ -10,7 +16,7 @@ use App\Models\PlanService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class PlanController extends Controller
+class PlanController extends Controller implements ModalityDomain, ModalityDefinition, ServiceDomain, ServiceDefinition, PriceDomain, PriceDefinition
 {
     public function index($idBussinessPartners) 
     {
@@ -21,21 +27,52 @@ class PlanController extends Controller
 
         foreach ($plans as $plan) {
             $modalities = PlanModality::whereNull('deleted_at')
-            ->select('id_modality', 'period', 'days')
+            ->select(
+                'id_modality', 'period', 'days',
+                DB::raw(
+                    ' CASE '.
+                        ' WHEN id_modality = '.self::MODALITY_ID_GENERAL.' THEN "'.self::MODALITY_NAME_GENERAL.'" '.
+                        ' WHEN id_modality = '.self::MODALITY_ID_BODYBUILDING.' THEN "'.self::MODALITY_NAME_BODYBUILDING.'" '.
+                        ' WHEN id_modality = '.self::MODALITY_ID_CROSSFIT.' THEN "'.self::MODALITY_NAME_CROSSFIT.'" '.
+                        ' WHEN id_modality = '.self::MODALITY_ID_FUNCTIONAL.' THEN "'.self::MODALITY_NAME_FUNCTIONAL.'" '.
+                    ' END as modality_name '
+                )
+            )
             ->where('id_plan', '=', $plan->id)
             ->get();
 
             $plan->modalities = $modalities;
 
             $services = PlanService::whereNull('deleted_at')
-            ->select('id_service')
+            ->select(
+                'id_service',
+                DB::raw(
+                    ' CASE '.
+                        ' WHEN id_service = '.self::SERVICE_ID_CABINET.' THEN "'.self::SERVICE_NAME_CABINET.'" '.
+                        ' WHEN id_service = '.self::SERVICE_ID_NUTRITIONIST.' THEN "'.self::SERVICE_NAME_NUTRITIONIST.'" '.
+                        ' WHEN id_service = '.self::SERVICE_ID_PARKING.' THEN "'.self::SERVICE_NAME_PARKING.'" '.
+                        ' WHEN id_service = '.self::SERVICE_ID_PHYSICAL_ASSESSMENT.' THEN "'.self::SERVICE_NAME_PHYSICAL_ASSESSMENT.'" '.
+                    ' END as service_name '
+                )
+            )
             ->where('id_plan', '=', $plan->id)
             ->get();
 
             $plan->services = $services;
 
             $prices = PlanPrice::whereNull('deleted_at')
-            ->select('period', 'price')
+            ->select(
+                'period', 'price',
+                DB::raw(
+                    ' CASE '.
+                        ' WHEN period = '.self::FREQUENCY_ID_MONTHLY.' THEN "'.self::FREQUENCY_NAME_MONTHLY.'" '.
+                        ' WHEN period = '.self::FREQUENCY_ID_BIMONTHLY.' THEN "'.self::FREQUENCY_NAME_BIMONTHLY.'" '.
+                        ' WHEN period = '.self::FREQUENCY_ID_QUARTERLY.' THEN "'.self::FREQUENCY_NAME_QUARTERLY.'" '.
+                        ' WHEN period = '.self::FREQUENCY_ID_SEMESTER.' THEN "'.self::FREQUENCY_NAME_SEMESTER.'" '.
+                        ' WHEN period = '.self::FREQUENCY_ID_YEARLY.' THEN "'.self::FREQUENCY_NAME_YEARLY.'" '.
+                    ' END as period_name '
+                )
+            )
             ->where('id_plan', '=', $plan->id)
             ->get();
 
